da06254b75f82379e9469fe1c807a3bbe8161e6a
Kevin Nguyen <nk279@hawaii.edu>
2021-10-31 21:07:34 -1000

    navbar in bootstrap

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,50 +3,54 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Roles } from 'meteor/alanning:roles';
 import { ROLE } from '../../api/role/Role';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
-const NavBar = ({ currentUser }) => {
-  const menuStyle = { marginBottom: '10px' };
-  return (
-    <Menu style={menuStyle} attached="top" borderless inverted>
-      <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/">
-        <Header inverted as='h1'>MATRP</Header>
-      </Menu.Item>
-      {currentUser ? (
-        [<Menu.Item id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-      ) : ''}
-      {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-        [<Menu.Item id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>,
-          <Dropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} item text="Manage" key="manage-dropdown">
-            <Dropdown.Menu>
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} exact to="/manage-database" content="Database" />
-            </Dropdown.Menu>
-          </Dropdown>]
-      ) : ''}
-      <Menu.Item position="right">
+const NavBar = ({ currentUser }) => (
+  <Navbar bg="dark" variant='dark'>
+    <Container fluid>
+      <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/" expand="lg">Pet Pickup</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="mr-auto">
+          {currentUser ? (
+            [<Nav.Link id={COMPONENT_IDS.NAVBAR_ADD_STUFF} as={NavLink} activeClassName="active" exact to="/add" key='add'>PAGE</Nav.Link>,
+              <Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF} as={NavLink} activeClassName="active" exact to="/list" key='list'>PAGE</Nav.Link>]
+          ) : ''}
+          {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
+            [<Nav.Link id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} activeClassName="active" exact to="/admin" key='admin'>PAGE</Nav.Link>,
+              <NavDropdown title='dropdown' id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} item text="Manage" key="manage-dropdown">
+                <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} exact to="/manage-database" content="Database">PAGE</NavDropdown.Item>
+              </NavDropdown>]
+          ) : ''}
+        </Nav>
         {currentUser === '' ? (
-          <Dropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} text="Login" pointing="top right" icon={'user'}>
-            <Dropdown.Menu>
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} icon="user" text="Sign In" as={NavLink} exact to="/signin" />
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_UP} icon="add user" text="Sign Up" as={NavLink} exact to="/signup" />
-            </Dropdown.Menu>
-          </Dropdown>
+          <Nav className="float-right">
+            <NavDropdown title={(<span>Login&nbsp;</span>)} align='end'>
+              <NavDropdown.Item as={NavLink} exact to="/signin">
+                &nbsp;Sign In
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} exact to="/signup">
+                Sign Up
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
         ) : (
-          <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right" icon={'user'}>
-            <Dropdown.Menu>
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Sign Out" as={NavLink} exact to="/signout" />
-            </Dropdown.Menu>
-          </Dropdown>
+          <Nav className="float-right">
+            <NavDropdown title={(<span>{this.props.currentUser}&nbsp;</span>)} align='end'>
+              <NavDropdown.Item as={NavLink} exact to="/signout">
+                  Sign Out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
         )}
-      </Menu.Item>
-    </Menu>
-  );
-};
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
 
 // Declare the types of all properties.
 NavBar.propTypes =
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
-import NotFound from '../pages/NotFound';
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
-import Signout from '../pages/Signout';
-import ManageDatabase from '../pages/ManageDatabase';
+// import ListStuff from '../pages/ListStuff';
+// import ListStuffAdmin from '../pages/ListStuffAdmin';
+// import AddStuff from '../pages/AddStuff';
+// import EditStuff from '../pages/EditStuff';
+// import NotFound from '../pages/NotFound';
+// import Signin from '../pages/Signin';
+// import Signup from '../pages/Signup';
+// import Signout from '../pages/Signout';
+// import ManageDatabase from '../pages/ManageDatabase';
 import { ROLE } from '../../api/role/Role';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
@@ -28,15 +28,15 @@ class App extends React.Component {
           <NavBar/>
           <Switch>
             <Route exact path="/" component={Landing}/>
-            <Route path="/signin" component={Signin}/>
-            <Route path="/signup" component={Signup}/>
-            <Route path="/signout" component={Signout}/>
-            <ProtectedRoute path="/list" component={ListStuff}/>
-            <ProtectedRoute path="/add" component={AddStuff}/>
-            <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-            <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
-            <AdminProtectedRoute path="/manage-database" component={ManageDatabase}/>
-            <Route component={NotFound}/>
+            {/* <Route path="/signin" component={Signin}/> */}
+            {/* <Route path="/signup" component={Signup}/> */}
+            {/* <Route path="/signout" component={Signout}/> */}
+            {/* <ProtectedRoute path="/list" component={ListStuff}/> */}
+            {/* <ProtectedRoute path="/add" component={AddStuff}/> */}
+            {/* <ProtectedRoute path="/edit/:_id" component={EditStuff}/> */}
+            {/* <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/> */}
+            {/* <AdminProtectedRoute path="/manage-database" component={ManageDatabase}/> */}
+            {/* <Route component={NotFound}/> */}
           </Switch>
           <Footer/>
         </div>
